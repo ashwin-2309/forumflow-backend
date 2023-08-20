03854f31fc1c811ef7605e8fa3adfe0472db40fd
Ashwin Kumar Singh <ashwinkumar2309@protonmail.com>
2023-08-21 03:32:44 +0530

    fixed the mongodb uri

--- connectMongoDb.js
+++ connectMongoDb.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose"; 
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, 
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://devbhanu19:GDAo2JD7EtNJVFwD@cluster0.n0sdowb.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
