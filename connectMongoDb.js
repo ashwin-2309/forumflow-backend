54b6f3dad0c15b2d4cd9f0e42bf615c876cca1c7
Ashwin Kumar Singh <ashwinkumar2309@protonmail.com>
2023-08-21 10:03:04 +0530

    fixed mongo

--- connectMongoDb.js
+++ connectMongoDb.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
+const mongoUri =
+  "mongodb+srv://devbhanu19:GDAo2JD7EtNJVFwD@cluster0.n0sdowb.mongodb.net/?retryWrites=true&w=majority";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://devbhanu19:GDAo2JD7EtNJVFwD@cluster0.n0sdowb.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const conn = await mongoose.connect(mongoUri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
